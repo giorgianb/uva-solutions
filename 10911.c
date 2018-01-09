bf039267fcb10d0e824eee3992e75d2baf19e9a7
Giorgian Borca-Tasciuc <giorgianb@openmailbox.org>
2018-01-09 11:50:24 -0500

    Rewrote solution to 10911 to use dynamic programming

--- 10911.c
+++ 10911.c
@@ -9,7 +9,10 @@
 #define __LOC__ __FILE__ ":" XSTR (__LINE__)
 #define __LOC_PRE__ __LOC__ ": "
 
+#define LENGTH(arr) (sizeof (arr) / sizeof (arr[0]))
+
 #define MAXNCASES 8
+#define UNSEEN -1
 
 
 struct coord
@@ -56,16 +59,32 @@ distance (const coord_t a, const coord_t b)
   return sqrt (dx * dx + dy * dy);
 }
 
+unsigned long long
+to_bit_mask (const bool used[MAXNCASES * 2], const size_t len)
+{
+  unsigned long long mask = 0;
+  for (size_t i = 0; i < len; ++i)
+    if (used[i])
+      mask |= (1 << i);
+
+  return mask;
+}
+
 double
 min_cost_helper (const coord_t houses[MAXNCASES * 2], const size_t nhouses,
-		 bool used[MAXNCASES * 2])
+		 bool used[MAXNCASES * 2], double seen[1 << (MAXNCASES * 2)])
 {
+  const unsigned long long mask = to_bit_mask (used, nhouses);
+
+  if (seen[mask] != UNSEEN)
+    return seen[mask];
+
   size_t start = 0;
   while (used[start] && start < nhouses)
     ++start;
 
   if (start == nhouses)
-    return 0;
+    return seen[mask] = 0;
 
   used[start] = true;
   double min_cost = DBL_MAX;
@@ -73,9 +92,8 @@ min_cost_helper (const coord_t houses[MAXNCASES * 2], const size_t nhouses,
     if (!used[i])
       {
 	used[i] = true;
-	const double c = (min_cost_helper (houses, nhouses, used)
+	const double c = (min_cost_helper (houses, nhouses, used, seen)
 			  + distance (houses[start], houses[i]));
-
 	min_cost = (c < min_cost) ? c : min_cost;
 
 	used[i] = false;
@@ -83,12 +101,16 @@ min_cost_helper (const coord_t houses[MAXNCASES * 2], const size_t nhouses,
 
   used[start] = false;
 
-  return min_cost;
+  return seen[mask] = min_cost;
 }
 
 double
 min_cost (const coord_t houses[MAXNCASES * 2], const size_t nhouses)
 {
   bool used[MAXNCASES * 2] = { 0 };
-  return min_cost_helper (houses, nhouses, used);
+  double seen[1 << (MAXNCASES * 2)];
+  for (size_t i = 0; i < LENGTH (seen); ++i)
+    seen[i] = UNSEEN;
+
+  return min_cost_helper (houses, nhouses, used, seen);
 }
